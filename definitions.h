95bb890cc8478f6e9ef3ee644b8dfc8c7fdad683
taanya <taanyanithyaanand@169-231-57-135.wireless.ucsb.edu>
2023-05-06 00:06:48 -0700

    added opcodes

--- definitions.h
+++ definitions.h
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define MEMORY_MAX (1 << 16)
+uint16_t memory[MEMORY_MAX];  /* 65536 locations */
+
 typedef enum registers {
     r_zero, //always zero
     r_at, // Assembler temporary
