ce6e06172124016957bc910bdaea6b6df52df2b1
taanya <taanyanithyaanand@169-231-58-167.wireless.ucsb.edu>
2023-05-23 16:37:31 -0700

    added nested loop test

---
+++ examples/medium/integers_nested_loops.cpp
@@ -0,0 +1,11 @@
+int main(){
+    int i,j;
+    int count=0;
+    for(i=0;i<5;i++){
+        for (j=0;j<7;j++){
+            count= count+1;
+        }
+    }
+    //final values -> count = 35
+    return 0;
+}
