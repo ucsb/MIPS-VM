26fa00a4475963b7786e60017bb947745125cdf4
taanya <taanyanithyaanand@169-231-58-167.wireless.ucsb.edu>
2023-05-24 14:05:32 -0700

    char test cases

---
+++ examples/medium/characters_arithmetic.cpp
@@ -0,0 +1,9 @@
+int main(){
+    char c1='a';
+    char c2=c1+2;
+
+    char c3=65;
+    char c4=c3+32;
+     //final value -> c2='c', c3='A', c4='a'
+    return 0;
+}
---
+++ examples/medium/characters_basic.cpp
@@ -0,0 +1,4 @@
+int main(){
+    char c=68;
+    return 0;
+}
